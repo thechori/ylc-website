dc395afed544485cf8b5eb7055b06ab8847d613e
teodizzo <thechori@gmail.com>
2015-10-09 22:10:25 -0500

    testing push to heroku

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+// set the port
+var port = process.env.PORT || 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// tell express to look in the public dir for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/pages/index.html'))
+  // ejs render automatically looks in the views folder
+  res.render('index');
+}
+
+// tell server which port to listen on
+app.listen(port, function() {
+  console.log('listening on ' + port);
 });
-
-
-app.listen(3000);
-console.log('YLC running at 3000');
